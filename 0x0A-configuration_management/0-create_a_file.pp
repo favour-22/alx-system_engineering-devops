0acc568e10779d5dc53e7cadd7ecfe86d3fd3fc3
favour-ayoade <anointed657@gmail.com>
2022-12-09 05:46:42 -0800

    Created a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file in /tmp using Puppet
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
